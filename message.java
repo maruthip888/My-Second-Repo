e14eeb66cafd79811059193fe93719a26e74f177
maruthip888 <maruthip266@gmail.com>
2025-09-03 22:00:25 +0530

    Add files via upload
    this is the message of the new students

---
+++ message.java
@@ -0,0 +1,13 @@
+package firstproject;
+
+public class message {
+	public static String getMessage() {
+		   return "Hiii,welcome to the java!";
+	   }
+		public static void main(String[] args) {
+			String data =getMessage();
+			System.out.println(data);
+			
+		}
+
+}
